6e51674063ae1da469d10ef59b9a0e65832d9903
jldupont <github@jldupont.com>
2010-09-10 11:02:49 -0400

    working on 7.0 with per-URL customization

--- src/content.js
+++ src/content.js
@@ -15,7 +15,8 @@ var default_begin_range=7*60;
 var default_end_range=20*60;
 
 // Context variables
-var state, timeout, randomize, active_time_range, begin_range, end_range;
+var state, timeout, randomize, active_time_range, begin_range, end_range, stick;
+var timer_id=null;
 
 function checkTimeRange() {
 	
@@ -30,16 +31,47 @@ function checkTimeRange() {
 	return (ct >= begin_range) && (ct <= end_range);
 }
 
+function maybeReload() {
+	//we already are waiting to be reloaded
+	if (state && timer_id) 
+		return;
+	
+	if (state===true || state=="true" || stick===true || stick==="true") {
+		// is there a "disabled_time_range" active?
+		if (active_time_range != false) {
+			if (checkTimeRange())
+				window.location.reload(true);
+			else
+				//recheck later
+				doSetTimeout();
+		} else {
+			window.location.reload(true);	
+		}
+		//console.log(" > auto-reload: scheduled in "+timeout+" ms.");
+	} else {
+		if (timer_id) {
+			clearInterval(timer_id);
+			timer_id=undefined;
+		}
+		//console.log(" > auto-reload: none scheduled");
+	}
+}
+
+function doSetTimeout() {
+	timer_id=setTimeout(maybeReload, timeout);
+}
+
 
 function reloader(cmd) {
 
-	state     = cmd.state     || false;
-	timeout   = cmd.timeout   || default_inter;
-	randomize = cmd.randomize || default_randomize;
-	active_time_range = cmd.active_time_range || default_active_time_range;
+	state     =   cmd.state       || false;
+	timeout   =   cmd.timeout     || default_inter;
+	randomize =   cmd.randomize   || default_randomize;
 	begin_range = cmd.begin_range || default_begin_range;
 	end_range   = cmd.end_range   || default_end_range;
-	stick = cmd.stick || false;
+	stick =       cmd.stick       || false;
+	
+	active_time_range = cmd.active_time_range || default_active_time_range;
 	
 	//console.log("Sticky: "+stick);
 	//console.log("Active Time Range: "+active_time_range);
@@ -52,7 +84,10 @@ function reloader(cmd) {
 	
 	//console.log("begin_range: "+begin_range+", end_range: "+end_range);
 		
+	if (timer_id)
+		doSetTimeout();
 	
+	/*
 	//we already are waiting to be reloaded
 	if (state && timer_id) 
 		return;
@@ -81,6 +116,7 @@ function reloader(cmd) {
 		}
 		//console.log(" > auto-reload: none scheduled");
 	}
+	*/
 }//
 
 port=chrome.extension.connect();
