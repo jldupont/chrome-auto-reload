b9c550350daeeb24e0b5fa148acf7ce9acf421d5
jldupont <github@jldupont.com>
2010-09-14 21:24:13 -0400

    oo options params

--- src/params.js
+++ src/params.js
@@ -4,17 +4,39 @@
  * 	@author: Jean-Lou Dupont
  */
 
-function UrlManager() {
-}
+function UrlManager() {}
 
 UrlManager.method("get", function(url, param, default_value){
 	var value=(localStorage[param+"."+url] || default_value);
-	if ((typeof value=="undefined") || (value=="undefined"))
+	if ((typeof rvalue=="undefined") || (rvalue=="undefined"))
 		value=default_value;
 	return value;
 });
 
 UrlManager.method("set", function(url, param, value){
 	localStorage[param+"."+url] = value;
+	return this;
+});
+
+
+function OptionsManager() {}
+
+OptionsManager.method("get", function(which, type, default_value) {
+	var rvalue=localStorage[which] || default_value;
+	var value=default_value;
+	try {
+		if (type=="int")
+			value=parseInt(rvalue);
+		if (type=="float")
+			value=parseFloat(rvalue);
+		if (type=="string")
+			value=rvalue;
+	} catch(err) {}
+	
+	return value;
+});
+
+OptionsManager.method("set", function(which, value){
+	localStorage[which]=value;
 	return this;
 });
